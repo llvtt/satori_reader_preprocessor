f330cba6cab3527e5cf12efb2427162a9c8c75ca
Luke Lovett <lukeharmonlovett+github@gmail.com>
2023-02-25 10:56:44 -0800

    add initial files

---
+++ main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import csv
+import sys
+
+
+def process_row(row):
+    expression = row[2]
+    reading = row[4]
+    gloss = row[7]
+    pos = row[8]
+
+    # Primary context, prefs readings inline.
+    primary_context = row[13]
+
+    # Secondary context, prefs readings inline.
+    secondary_context = row[19].strip()
+    tertiary_context = row[25].strip()
+    additional_contexts = []
+    if secondary_context:
+        additional_contexts.append(secondary_context)
+    if tertiary_context:
+        additional_contexts.append(tertiary_context)
+    additional_context_markup = ''.join(
+        f'<div class="additional-context">{context}</div>'
+        for context in additional_contexts
+    )
+
+    # Create glossary section.
+    glossary = (
+        f'{additional_context_markup}'
+        f'<div class="parts-of-speech">{pos}</div>'
+        f'<div class="glossary">{gloss}</div>'
+    )
+
+    return (
+        expression,
+        reading,
+        glossary,
+        primary_context,
+    )
+
+
+
+def process_rows(rows):
+    for row in rows:
+        yield process_row(row)
+
+
+def main(ifile, ofile):
+    with open(ifile, 'r') as infile:
+        reader = csv.reader(infile)
+        # Skip header line.
+        next(reader)
+
+        with open(ofile, 'w') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerows(process_rows(reader))
+
+    print('Done')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print(f'USAGE: {sys.argv[0]} <input filename> <output filename>')
+        exit(1)
+
+    main(sys.argv[1], sys.argv[2])
